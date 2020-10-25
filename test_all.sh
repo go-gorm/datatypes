1d446e9a046674d0ad29c703cf7aae5185144a1f
Jinzhu <wosmvp@gmail.com>
2020-10-25 14:16:49 +0800

    Fix test_all script

--- test_all.sh
+++ test_all.sh
@@ -3,5 +3,8 @@
 dialects=("postgres" "postgres_simple" "mysql" "mssql" "sqlite")
 
 for dialect in "${dialects[@]}" ; do
+  if [ "$GORM_DIALECT" = "" ] || [ "$GORM_DIALECT" = "${dialect}" ]
+  then
     GORM_DIALECT=${dialect} go test  --tags "json1"
+  fi
 done
