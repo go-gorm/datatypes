413158330a6a7e281901f0e41a1902e71e26a96d
masaushi <19667785+masaushi@users.noreply.github.com>
2021-11-15 12:48:30 +0900

    Add time data type support  (#89)
    * Add datatypes.Time
    
    * Update README.md

---
+++ time_test.go
@@ -0,0 +1,35 @@
+package datatypes_test
+
+import (
+	"fmt"
+	"testing"
+
+	"gorm.io/datatypes"
+	. "gorm.io/gorm/utils/tests"
+)
+
+func TestTime(t *testing.T) {
+	if SupportedDriver("mysql", "postgres", "sqlite", "sqlserver") {
+		type UserWithTime struct {
+			ID   uint
+			Name string
+			Time datatypes.Time
+		}
+
+		DB.Migrator().DropTable(&UserWithTime{})
+		if err := DB.Migrator().AutoMigrate(&UserWithTime{}); err != nil {
+			t.Fatalf("failed to migrate, got error: %v", err)
+		}
+
+		user := UserWithTime{Name: "user1", Time: datatypes.NewTime(1, 2, 3, 0)}
+		DB.Create(&user)
+
+		result := UserWithTime{}
+		if err := DB.First(&result, "name = ? AND time = ?", "user1", datatypes.NewTime(1, 2, 3, 0)).Error; err != nil {
+			t.Fatalf("failed to find record with time, got error: %v", err)
+		}
+
+		fmt.Println(result)
+		AssertEqual(t, result.Time, datatypes.NewTime(1, 2, 3, 0))
+	}
+}
