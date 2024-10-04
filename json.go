85748e09cc403e6ed985ba67afd55a9a1c6fc809
jia.zheng2 <jia.zheng2@bytedance.com>
2024-10-04 17:29:35 +0800

    feat: support using path for JSON_CONTAINS

--- json.go
+++ json.go
@@ -451,11 +451,13 @@ func JSONArrayQuery(column string) *JSONArrayExpression {
 
 type JSONArrayExpression struct {
 	column      string
+	keys        []string
 	equalsValue interface{}
 }
 
-func (json *JSONArrayExpression) Contains(value interface{}) *JSONArrayExpression {
+func (json *JSONArrayExpression) Contains(value interface{}, keys ...string) *JSONArrayExpression {
 	json.equalsValue = value
+	json.keys = keys
 	return json
 }
 
@@ -464,9 +466,14 @@ func (json *JSONArrayExpression) Build(builder clause.Builder) {
 	if stmt, ok := builder.(*gorm.Statement); ok {
 		switch stmt.Dialector.Name() {
 		case "mysql":
-			builder.WriteString("JSON_CONTAINS (" + stmt.Quote(json.column) + ", JSON_ARRAY(")
+			builder.WriteString("JSON_CONTAINS(" + stmt.Quote(json.column) + ",JSON_ARRAY(")
 			builder.AddVar(stmt, json.equalsValue)
-			builder.WriteString("))")
+			builder.WriteByte(')')
+			if len(json.keys) > 0 {
+				builder.WriteByte(',')
+				builder.AddVar(stmt, jsonQueryJoin(json.keys))
+			}
+			builder.WriteByte(')')
 		case "sqlite":
 			builder.WriteString("exists(SELECT 1 FROM json_each(" + stmt.Quote(json.column) + ") WHERE value = ")
 			builder.AddVar(stmt, json.equalsValue)
